29fb23713e1760339d717f98327cbc4fdd9fb295
andrej <andrej@digitouch.lt>
2018-06-01 01:01:41 +0300

    layout

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import logo from './../../logo.svg';
 import './App.css';
 
 import Header from './../header/Header';
+import CustomForm from './../form/Form';
 
 class App extends Component {
     render() {
@@ -11,17 +12,59 @@ class App extends Component {
             <div className="App">
                 <Header />
 
+                <div className="container layout">
+                    <section className="layout__column layout__column--twoHrirds">
+                        <article>
+                            <h2>Aenean ac diam  nec neque fringilla</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis autem.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis autem.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis autem.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute .</p>
+                        </article>
 
-                <div className="container">
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
+                        <div className="layout__column layout__column--half">
+                            <article>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                            </article>
+                        </div>
+
+                        <div className="layout__column layout__column--half">
+                            <article>
+                                <h3>Aenean ac diam  nec neque fringilla</h3>
+
+                                <ul>
+                                    <li><a href="#">Lorem ipsum dolor</a></li>
+                                    <li><a href="#">sit amet, consectetur</a></li>
+                                    <li><a href="#">adipisicing elit</a></li>
+                                </ul>
+                            </article>
+                        </div>
+                    </section>
+
+                    <aside className="layout__column layout__column--thirdReduced">
+                        <article>
+                            <h3>Aenean ac diam  nec neque fringilla</h3>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                        </article>
+
+                        <CustomForm />
+                    </aside>
                 </div>
 
+                <section className="container layout highlights">
+                    <div className="layout__column layout__column--third">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                    <div className="layout__column layout__column--third">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                    <div className="layout__column layout__column--third">
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                </section>
             </div>
         );
     }
-
 }
 
 export default App;
---
+++ src/components/form/Form.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// import './Header.css';
+// import './Dropdown.css';
+
+class CustomForm extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <section>
+                
+                <p>this is form</p>
+            </section>
+        );
+    }
+}
+
+export default CustomForm;
